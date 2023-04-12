80dbe2e79c140b1beebfe0122fb1770e0bc1e5f0
Nihan Cj <nihancj.com@gmail.com>
2023-04-12 15:33:58 +0530

    feat(hooks): A function executed after loading
    Hooks can be defined for each profile during setup using
    hooks = {<profile1> = fun(is_on_startup:bool), <profile2> = ...}
    Replaces the hardcoded 'LspStart'

--- lua/lazy-plugin-switcher/init.lua
+++ lua/lazy-plugin-switcher/init.lua
@@ -2,11 +2,13 @@ local M = {}
 
 M.active_profile = ''
 M.core_plugins = {}
+M.core_hooks = {}
 M.io = require 'lazy-plugin-switcher.io'
 
 M.setup = function (opts)
 	for _, v in ipairs(opts["profiles"]) do
 		M.core_plugins[v] = opts[v]
+		M.core_hooks[v] = opts.hooks[v]
 	end
 
 	M.on_startup()
@@ -15,25 +17,30 @@ end
 M.toggle_profile = function (profile)
 	if profile == M.active_profile then
 		M.active_profile = ''
+		vim.notify("Profile deacivated: " .. profile)
 		return
 	elseif M.core_plugins[profile] == nil or M.core_plugins[profile] == '' then
-		print("Invalid Profile")
+		vim.notify("Invalid Profile")
 		return
 	end
-	M.load_profile(profile)
+	vim.notify("Profile acivated: " .. profile)
+	M.load_profile(profile, false)
 end
 
-M.load_profile = function (profile)
+M.load_profile = function (profile, is_on_startup)
 	M.active_profile = profile
 	local plugins = { plugins = M.core_plugins[profile] }
 	require('lazy').load(plugins)
-	vim.cmd "LspStart"
+
+	if M.core_hooks[profile] ~= nil then
+		M.core_hooks[profile](is_on_startup)
+	end
 end
 
 M.on_startup = function ()
 	local profile = M.io.read()
 	if profile == nil or  profile == '' then return end
-	M.load_profile(profile)
+	M.load_profile(profile, true)
 end
 
 M.on_exit = function ()
