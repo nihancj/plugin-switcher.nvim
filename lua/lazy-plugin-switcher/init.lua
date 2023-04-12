cd8db0cb42cf5291e26c0d6fb85c4c3a37fd2e85
Nihan Cj <nihancj.com@gmail.com>
2023-04-12 23:00:09 +0530

    fix(): generate auto/user commands only if called
    The user command and the auto command to save active_profile is only
    created if the plugin is called using setup(). This is to not show a
    useless command and to prevent unnessary write operations respectively.

--- lua/lazy-plugin-switcher/init.lua
+++ lua/lazy-plugin-switcher/init.lua
@@ -5,12 +5,32 @@ M.core_plugins = {}
 M.core_hooks = {}
 M.io = require 'lazy-plugin-switcher.io'
 
+local gen_commands = function ()
+	-- creating user command
+	vim.api.nvim_create_user_command("SwitchPlugins", function(opts)
+		require("lazy-plugin-switcher").toggle_profile(opts.fargs[1])
+	end, {
+		nargs = 1,
+		complete = function(_, _, _)
+			return M.config.profiles
+		end,
+	})
+
+	-- create auto command to save current session on exit
+	vim.api.nvim_create_autocmd("VimLeavePre", {
+		group = vim.api.nvim_create_augroup("lazy-plugin-switcher", { clear = true }),
+		command = 'lua require("lazy-plugin-switcher").on_exit()',
+	})
+end
+
 M.setup = function (opts)
+	M.config = opts
 	for _, v in ipairs(opts["profiles"]) do
 		M.core_plugins[v] = opts.plugins[v]
 		M.core_hooks[v] = opts.hooks[v]
 	end
 
+	gen_commands()
 	M.on_startup()
 end
 
@@ -37,7 +57,9 @@ M.load_profile = function (profile, is_on_startup)
 	end
 end
 
+
 M.on_startup = function ()
+	-- reading previous session data
 	local profile = M.io.read()
 	if profile == nil or  profile == '' then return end
 	M.load_profile(profile, true)
