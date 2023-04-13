f3f6033198967fdc836fff958345ad30f41bd198
Nihan Cj <nihancj.com@gmail.com>
2023-04-13 22:08:54 +0530

    refactor: cleaner code and meaningul names
    Improves code readability.

--- lua/lazy-plugin-switcher/init.lua
+++ lua/lazy-plugin-switcher/init.lua
@@ -1,11 +1,18 @@
 local M = {}
 
-M.active_profile = ''
+M.active_profile = ""
 M.core_plugins = {}
 M.core_hooks = {}
-M.io = require 'lazy-plugin-switcher.io'
+M.io = require("lazy-plugin-switcher.io")
 
-local gen_commands = function ()
+local is_valid_string = function(string)
+	if string == nil or string == '' then
+		return false
+	end
+	return true
+end
+
+local gen_commands = function()
 	-- creating user command
 	vim.api.nvim_create_user_command("SwitchPlugins", function(opts)
 		require("lazy-plugin-switcher").toggle_profile(opts.fargs[1])
@@ -23,7 +30,43 @@ local gen_commands = function ()
 	})
 end
 
-M.setup = function (opts)
+M.on_exit = function()
+	M.io.write(M.active_profile)
+end
+
+M.load_profile = function(profile, is_on_startup)
+	M.active_profile = profile
+	local plugins = { plugins = M.core_plugins[profile] }
+	require("lazy").load(plugins)
+
+	if type(M.core_hooks[profile]) == "function" then
+		M.core_hooks[profile](is_on_startup)
+	end
+end
+
+M.toggle_profile = function(profile_name)
+	if profile_name == M.active_profile then
+		M.active_profile = ""
+		vim.notify("Profile deacivated: " .. profile_name)
+		return
+	elseif not is_valid_string(M.core_plugins[profile_name]) then
+		vim.notify("Invalid Profile")
+		return
+	end
+	vim.notify("Profile acivated: " .. profile_name)
+	M.load_profile(profile_name, false)
+end
+
+M.on_startup = function()
+	-- fetching previous profile and loading it
+	local prev_profile = M.io.read()
+	if not is_valid_string(prev_profile) then
+		return
+	end
+	M.load_profile(prev_profile, true)
+end
+
+M.setup = function(opts)
 	M.config = opts
 	for _, v in ipairs(opts["profiles"]) do
 		M.core_plugins[v] = opts.plugins[v]
@@ -32,41 +75,6 @@ M.setup = function (opts)
 
 	gen_commands()
 	M.on_startup()
-end
-
-M.toggle_profile = function (profile)
-	if profile == M.active_profile then
-		M.active_profile = ''
-		vim.notify("Profile deacivated: " .. profile)
-		return
-	elseif M.core_plugins[profile] == nil or M.core_plugins[profile] == '' then
-		vim.notify("Invalid Profile")
-		return
-	end
-	vim.notify("Profile acivated: " .. profile)
-	M.load_profile(profile, false)
-end
-
-M.load_profile = function (profile, is_on_startup)
-	M.active_profile = profile
-	local plugins = { plugins = M.core_plugins[profile] }
-	require('lazy').load(plugins)
-
-	if M.core_hooks[profile] ~= nil then
-		M.core_hooks[profile](is_on_startup)
-	end
-end
-
-
-M.on_startup = function ()
-	-- reading previous session data
-	local profile = M.io.read()
-	if profile == nil or  profile == '' then return end
-	M.load_profile(profile, true)
-end
-
-M.on_exit = function ()
-	M.io.write(M.active_profile)
 end
 
 return M
