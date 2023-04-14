a9d7abd10c686da6d81346743d57acf98e9963a0
Nihan Cj <nihancj.com@gmail.com>
2023-04-14 20:00:06 +0530

    fix(load_on_ft): bugs and performance issues

--- lua/lazy-plugin-switcher/init.lua
+++ lua/lazy-plugin-switcher/init.lua
@@ -1,14 +1,25 @@
 local M = {}
 
-M.io = require("lazy-plugin-switcher.io")
-M.config = { profiles = {}, plugins = {}, hooks = {}, ft = {} }
+local au_group = vim.api.nvim_create_augroup("lazy-plugin-switcher", { clear = true })
+local io = require("lazy-plugin-switcher.io")
 M.active_profile = ""
+M.filetype_mapping = {}
+M.config = { profiles = {}, plugins = {}, hooks = {}, ft = {} }
 
-local is_valid_string = function(string)
-	if string == nil or string == "" then
+local is_valid = function(value)
+	if value == nil or value == "" or value == {} then
 		return false
 	end
 	return true
+end
+
+local gen_filetype_mapping = function()
+	for profile, filetypes in pairs(M.config.ft) do
+		for _, filetype in ipairs(filetypes) do
+			M.filetype_mapping[filetype] = M.filetype_mapping[filetype] or {}
+			table.insert(M.filetype_mapping[filetype], profile)
+		end
+	end
 end
 
 local gen_commands = function()
@@ -23,25 +34,29 @@ local gen_commands = function()
 	})
 
 	-- auto command to save current session on exit
-	local au_group = vim.api.nvim_create_augroup("lazy-plugin-switcher", { clear = true })
 	vim.api.nvim_create_autocmd("VimLeavePre", {
 		group = au_group,
 		command = 'lua require("lazy-plugin-switcher").on_exit()',
 	})
 	-- auto command to check buffer filetype
-	vim.api.nvim_create_autocmd("BufEnter", {
-		group = au_group,
-		command = 'lua require("lazy-plugin-switcher").check_buf_ft()',
-	})
+	if is_valid(M.config.ft) then
+		vim.api.nvim_create_autocmd("BufEnter", {
+			group = au_group,
+			command = 'lua require("lazy-plugin-switcher").check_buf_ft()',
+		})
+	end
 end
 
 M.on_exit = function()
-	M.io.write(M.active_profile)
+	io.write(M.active_profile)
 end
 
 M.load_profile = function(profile, is_on_startup)
-	local plugins = { plugins = M.config.plugins[profile] }
-	require("lazy").load(plugins)
+	if profile == M.active_profile or not is_valid(M.config.plugins[profile]) then
+		return
+	end
+	M.active_profile = profile
+	require("lazy").load({ plugins = M.config.plugins[profile] })
 
 	if type(M.config.hooks[profile]) == "function" then
 		M.config.hooks[profile](is_on_startup)
@@ -53,42 +68,39 @@ M.toggle_profile = function(profile_name)
 		M.active_profile = ""
 		vim.notify("Profile deacivated: " .. profile_name)
 		return
-	elseif not is_valid_string(M.config.plugins[profile_name]) then
+	elseif not is_valid(M.config.plugins[profile_name]) then
 		vim.notify("Invalid Profile")
 		return
 	end
 	vim.notify("Profile acivated: " .. profile_name)
-	M.active_profile = profile_name
 	M.load_profile(profile_name, false)
 end
 
 M.check_buf_ft = function()
 	local current_filetype = vim.api.nvim_buf_get_option(0, "filetype")
-	for profile, profile_fts in pairs(M.config.ft) do
-		for _, profile_ft in ipairs(profile_fts) do
-			if current_filetype == profile_ft then
-				M.load_profile(profile)
-				M.config.ft[profile] = {}
-				break
-			end
+	M.filetype_mapping[current_filetype] = M.filetype_mapping[current_filetype] or nil
+	if is_valid(M.filetype_mapping[current_filetype]) then
+		for _, profile in pairs(M.filetype_mapping[current_filetype]) do
+			M.load_profile(profile)
 		end
+		M.filetype_mapping[current_filetype] = nil
 	end
 end
 
 M.on_startup = function()
-	local prev_profile = M.io.read()
-	if not is_valid_string(prev_profile) then
+	local prev_profile = io.read()
+	if not is_valid(prev_profile) then
 		return
 	end
-	M.active_profile = prev_profile
 	M.load_profile(prev_profile, true)
 end
 
 M.setup = function(user_opts)
-   for option, value in pairs(user_opts) do
-      M.config[option] = value
-   end
+	for option, value in pairs(user_opts) do
+		M.config[option] = value
+	end
 
+	gen_filetype_mapping()
 	gen_commands()
 	M.on_startup()
 end
